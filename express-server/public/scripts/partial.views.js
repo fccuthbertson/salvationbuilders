c5771bcbdc32e2f69b260ea8752848bcc06fe53c
frank cuthbertson <fccuthbertson@gmail.com>
2023-08-20 15:41:20 -0500

    using shared nav

---
+++ express-server/public/scripts/partial.views.js
@@ -0,0 +1,7 @@
+    (function() {
+    $(window).ready(function() {
+        $('#header').load('header.html')
+        $('#navigation').load('nav.html')
+        $('#footer').load('footer.html')
+    })
+})()
