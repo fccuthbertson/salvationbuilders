f178b5ca2bcf4af7af21042881527c4f0f10a8c7
frank cuthbertson <fccuthbertson@gmail.com>
2023-10-08 11:43:27 -0500

    temp removing 'services' nav section

--- express-server/aws/credentials.js
+++ express-server/aws/credentials.js
@@ -1,17 +1,9 @@
-export class AwsCredentials {
-    constructor() {
-        this.region = process.env.AWS_REGION
-        this.awsKey = process.env.AWS_KEY
-        this.awsSecret = process.env.AWS_SECRET
-    }
-}
-
 export class ClientConfig {
     constructor() {
         this.region = process.env.AWS_REGION
         this.credentials = {
-            accessKeyId: process.env.AWS_KEY,
-            secretAccessKey: process.env.AWS_SECRET
+            accessKeyId: process.env.AWS_ACESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
         }
     }
 }
